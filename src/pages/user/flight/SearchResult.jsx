0feaa2d2bb56c8b3ec3e616caaaeaef2774f747d
Kamilah Syahrabanu <kamilahsyahrabanu@gmail.com>
2024-06-28 13:47:05 +0700

    Ketinggalan dikit

--- src/pages/user/flight/SearchResult.jsx
+++ src/pages/user/flight/SearchResult.jsx
@@ -671,9 +671,9 @@ export default function SearchResult() {
                       >
                         <path
                           stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="m1 9 4-4-4-4"
                         />
                       </svg>
